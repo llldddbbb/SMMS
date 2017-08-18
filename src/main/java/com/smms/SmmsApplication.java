7258c6dc2c16ce5e67b942a37a28adecaa09205a
llldddbbb <571002217@qq.com>
2017-08-18 22:00:17 +0800

    项目结构初始化

---
+++ src/main/java/com/smms/SmmsApplication.java
@@ -0,0 +1,12 @@
+package com.smms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmmsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/smms/SmmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
